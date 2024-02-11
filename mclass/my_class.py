1e4db967901a9369951d9cd4f11fba1db137c47a
LS <lislmaka@gmail.com>
2024-02-11 10:41:03 +0300

    add my class

---
+++ mclass/my_class.py
@@ -0,0 +1,7 @@
+class andrey:
+    def __init__(self, name="John", age=36):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
